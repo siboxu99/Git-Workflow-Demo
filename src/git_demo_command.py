ba1d5cd02e8a711b09e58f5ffaa3faa7dc7560b3
Sibo Xu <Sibo.Xu@riotinto.com>
2025-03-20 11:57:25 +0800

    final prep for the gitgood session

---
+++ src/git_demo_command.py
@@ -0,0 +1,3 @@
+class GitDemoClass:
+    def hello_world(self):
+        print('This class will be used for Git Good Demo session!')
--- src/git_workflow.py
+++
@@ -1,6 +0,0 @@
-def hello_world():
-    print('Hello world from the main branch')
-
-
-def hello_world_from_main_branch():
-    print('Hello world from the main branch')
